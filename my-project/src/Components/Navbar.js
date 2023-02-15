35110a97a007f11d792b4dc7dd11ba45c8fdf8a4
Alexander Alfon <87632745+AAlfon@users.noreply.github.com>
2023-02-15 13:42:41 +0800

    test-layout

--- my-project/src/Components/Navbar.js
+++ my-project/src/Components/Navbar.js
@@ -5,31 +5,35 @@ import "../styles/Navbar.css";
 
 
 
+
 function Navbar() {
   return (
   
     <div className='nav-bar'>
+       
+             
         <div className='logo'>
+       
             <img className='image-logo' src={Logo} alt="sample logo" />
         </div>
+        
         <div className='list'>
+        
             <ul className="nav justify-content-end">
                     <li className="nav-item" >
-                        <Link to="/" Name="nav-link activeclass" aria-current="page">Home</Link>
+                        <Link to="/" Name="nav-link activeclass" aria-current="page" style={{ textDecoration: 'none'}}>Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="menu" Name="nav-link activeclass" aria-current="page">Menu</Link>
+                        <Link to="menu" Name="nav-link activeclass" aria-current="page" style={{ textDecoration: 'none'}}>Menu</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="about" Name="nav-link activeclass" aria-current="page">About Us</Link>
+                        <Link to="about" Name="nav-link activeclass" aria-current="page" style={{ textDecoration: 'none'}}>About Us</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="contact" Name="nav-link activeclass" aria-current="page">Contact Us</Link>
+                        <Link to="contact" Name="nav-link activeclass" aria-current="page" style={{ textDecoration: 'none'}}>Contact Us</Link>
                     </li>
                 </ul>
         </div>
-
-
      
     </div>
   )
--- my-project/src/pages/Home.js
+++ my-project/src/pages/Home.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from 'react';
+
+
+
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is the Home Page</h1>
+    <div className='home-page'>
+      <h1> Search for you favorite recipe</h1>
+      <div className='search'>
+      
+          <input className='search-bar' type="text" id="recipesearch" placeholder="Search for Recipe" />
+             <button id="searchBtn">Search</button>
+      </div>
+
+      <div className='categories container text-center'>
+          <h4>Categories</h4>
+          <div className="row justify-content-start categories-content">
+              <div className="col-4">
+                One of two columns
+              </div>
+              <div className="col-4">
+                One of two columns
+              </div>
+          </div>
+            <div className="row justify-content-center categories-content">
+              <div className="col-4">
+                One of two columns
+              </div>
+              <div className="col-4">
+                One of two columns
+              </div>
+            </div>
+            <div className="row justify-content-end categories-content">
+              <div className="col-4">
+                One of two columns
+              </div>
+              <div className="col-4">
+                One of two columns
+              </div>
+            </div>
+            <div className="row justify-content-around categories-content">
+              <div className="col-4">
+                One of two columns
+              </div>
+              <div className="col-4">
+                One of two columns
+              </div>
+            </div>
+            <div className="row justify-content-between categories-content">
+              <div className="col-4">
+                One of two columns
+              </div>
+              <div className="col-4">
+                One of two columns
+              </div>
+            </div>
+            <div className="row justify-content-evenly categories-content">
+              <div className="col-4">
+                One of two columns
+              </div>
+              <div className="col-4">
+                One of two columns
+              </div>
+            </div>
+
+         
+         
+      </div>
+        
+        <div>
+          <h1>hello</h1>
+        </div>
+        
     </div>
   )
 }
