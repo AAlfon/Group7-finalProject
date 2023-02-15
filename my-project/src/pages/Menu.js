de221d67c24d0d944c86260ed1ea9e62086c9884
Alma Cleto <emscleto11@gmail.com>
2023-02-15 13:47:09 +0800

    fixed travel page

--- my-project/src/pages/Menu.js
+++ my-project/src/pages/Menu.js
@@ -6,71 +6,71 @@ const Menu = () => {
             <h1 className='section-header'>Sample Menu Page</h1>
             <main>
 
-  <div class="container marketing">
+  <div className="container marketing">
 
-    <div class="row">
-      <div class="col-lg-4">
-      <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn1.matadornetwork.com/blogs/1/2021/04/bananacue-filipino-desserts.jpg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false" />
+    <div className="row">
+      <div className="col-lg-4">
+      <img className="bd-placeholder-img rounded-circle" width="150" height="150" src="https://cdn1.matadornetwork.com/blogs/1/2021/04/bananacue-filipino-desserts.jpg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false" />
 
         <h2>Banana Cue</h2>
         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
       </div>
-      <div class="col-lg-4">
-      <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn1.matadornetwork.com/blogs/1/2021/04/Puto-bumbong-filiipino-dessert.jpg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false" />
+      <div className="col-lg-4">
+      <img className="bd-placeholder-img rounded-circle" width="150" height="150" src="https://cdn1.matadornetwork.com/blogs/1/2021/04/Puto-bumbong-filiipino-dessert.jpg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false" />
 
         <h2>Puto bumbong</h2>
         <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
       </div>
-      <div class="col-lg-4">
-      <img class="bd-placeholder-img rounded-circle" width="140" height="140" src="https://cdn1.matadornetwork.com/blogs/1/2021/04/Ginataang-Bilo-Bilo-filipino-dessert.jpg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false" />
+      <div className="col-lg-4">
+      <img className="bd-placeholder-img rounded-circle" width="150" height="150" src="https://cdn1.matadornetwork.com/blogs/1/2021/04/Ginataang-Bilo-Bilo-filipino-dessert.jpg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false" />
 
         <h2>Ginataang bilo-bilo</h2>
         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+        <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
       </div>
     </div>
 
 
-    <hr class="featurette-divider" />
+    <hr className="featurette-divider" />
 
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-        <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+    <div className="row featurette">
+      <div className="col-md-7">
+        <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
+        <p className="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
       </div>
-      <div class="col-md-5">
-      <img src="https://www.filipino-recipes-lutong-pinoy.com/images/xpaella-valenciana-filipino-recipes-lutong-pinoy.com.jpg.pagespeed.ic.6lZhl1ijqd.jpg" class="d-block w-100" alt="..." />
-
-      </div>
-    </div>
-
-    <hr class="featurette-divider" />
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-      <img src="https://www.lutongbahayrecipe.com/wp-content/uploads/2019/03/pork-chicken-adobo-lutong-bahay-640x381.jpg" class="d-block w-100" alt="..." />
+      <div className="col-md-5">
+      <img src="https://www.filipino-recipes-lutong-pinoy.com/images/xpaella-valenciana-filipino-recipes-lutong-pinoy.com.jpg.pagespeed.ic.6lZhl1ijqd.jpg" className="d-block w-100" alt="..." />
 
       </div>
     </div>
 
-    <hr class="featurette-divider"/>
+    <hr className="featurette-divider" />
 
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-        <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+    <div className="row featurette">
+      <div className="col-md-7 order-md-2">
+        <h2 className="featurette-heading">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
+        <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
       </div>
-      <div class="col-md-5">
-      <img src="https://thecommhq.files.wordpress.com/2014/10/food8.png" class="d-block w-100" alt="..." />
+      <div className="col-md-5 order-md-1">
+      <img src="https://www.lutongbahayrecipe.com/wp-content/uploads/2019/03/pork-chicken-adobo-lutong-bahay-640x381.jpg" className="d-block w-100" alt="..." />
+
       </div>
     </div>
-    <hr class="featurette-divider" />
+
+    <hr className="featurette-divider"/>
+
+    <div className="row featurette">
+      <div className="col-md-7">
+        <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
+        <p className="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+      </div>
+      <div className="col-md-5">
+      <img src="https://thecommhq.files.wordpress.com/2014/10/food8.png" className="d-block w-100" alt="..." />
+      </div>
+    </div>
+    <hr className="featurette-divider" />
   </div>
 </main>
         </div>
