a5178f56f05849966594b0da26887d2cce5eca7d
Alma Cleto <emscleto11@gmail.com>
2023-02-15 13:01:04 +0800

    updated

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -5,8 +5,8 @@ import Home from "./pages/Home";
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Menu from './pages/Menu';
-import Travel from './pages/Travel'
-
+import Travel from './pages/Travel';
+import Footer from './Components/Footer';
 
 
 const App = () => {
@@ -14,14 +14,13 @@ const App = () => {
       <div className='App'>
 <Navbar />
         <Routes>
-         
             <Route path="/" element={<Home/>} />
             <Route path='menu' element={<Menu/>} />
             <Route path='travel' element={<Travel/>} />
             <Route path='about' element={<About/>} />
-            <Route path='contact' element={<Contact/>} />
+            <Route path='contact-us' element={<Contact/>} />
         </Routes>
-        
+<Footer />
       </div>
 
   );
--- my-project/src/Components/Navbar.js
+++ my-project/src/Components/Navbar.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Logo from '../assets/logo.png';
-import { Link } from "react-router-dom";
-import "../styles/Navbar.css";
+import { Link } from 'react-router-dom';
 
-
-
-function Navbar() {
-  return (
-    <>
-
-    <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-  <Link to="/" className="navbar-brand"><img className='image-logo' src={Logo} alt="sample logo" /></Link>
+const Navbar = () => {
+    return (
+        <div>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+  <Link to="/" className="navbar-brand">
+  {/* <img src="https://picsum.photos/200/300" alt="" width="50" height="34" class="d-inline-block align-text-top" /> */}
+  LOGO
+  </Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -18,25 +16,26 @@ function Navbar() {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/" className="nav-link">Home </Link>
       </li>
       <li className="nav-item">
-        <Link to="menu" className="nav-link">Menu</Link>
+        <Link to="/menu" className="nav-link" >Menu</Link>
       </li>
       <li className="nav-item">
-        <Link to="travel" className="nav-link">Travel</Link>
+        <Link to="/travel" className="nav-link" >Travel</Link>
       </li>
       <li className="nav-item">
-        <Link to="about" className="nav-link">About Us</Link>
+        <Link to="/about" className="nav-link">About Us</Link>
       </li>
       <li className="nav-item">
-        <Link to="contact" className="nav-link">Contact Us</Link>
+        <Link to="/contact-us" className="nav-link">Contact Us</Link>
       </li>
     </ul>
   </div>
 </nav>
-    </>
-  )
+</div>
+
+    );
 }
 
-export default Navbar
+export default Navbar;
