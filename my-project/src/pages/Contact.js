8a312d3ed66cb2145cf4004f866b0b0eff9be801
Hilarry4269 <118233630+Hilarry4269@users.noreply.github.com>
2023-02-16 20:09:47 -0800

    Update Contact.js

--- my-project/src/pages/Contact.js
+++ my-project/src/pages/Contact.js
@@ -3,14 +3,19 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className='contact-page'>
-            <h1 className='section-header'>Sample Contact Us Page</h1>
+            <h1 className='section-header'></h1>
 
         <div class="form-area mb-5">
         <div class="container">
             <div class="row single-form g-0">
                 <div class="col-sm-12 col-lg-6">
                     <div class="left">
-                        <h2><span>Want to know more about your favorite food?</span> <br />Contact us now!</h2>
+                        <h2><span>Welcome to our food recipe website! We're thrilled to have you here, and we can't wait to share some delicious and easy-to-follow recipes with you.
+                            </span> <br />Contact us now!</h2>
+                            <h2><span>Welcome to our food recipe website! We're thrilled to have you here, and we can't wait to share some delicious and easy-to-follow recipes with you.
+                            </span> <br />Contact us now!</h2>
+                            <h2><span>Welcome to our food recipe website! We're thrilled to have you here, and we can't wait to share some delicious and easy-to-follow recipes with you.
+                            </span> <br />Contact us now!</h2>
                     </div>
                 </div>
                 <div class="col-sm-12 col-lg-6">
