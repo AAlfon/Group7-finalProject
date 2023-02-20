39aa2c325aee3bd35b2865051bd7131edac87745
Alexander Alfon <87632745+AAlfon@users.noreply.github.com>
2023-02-20 16:44:10 +0800

    css update

--- my-project/src/pages/Home.js
+++ my-project/src/pages/Home.js
@@ -122,7 +122,7 @@ const Home = () => {
             <div className="box">
               <img src="https://i.pinimg.com/736x/6c/52/81/6c528187700fb46afda2b184e7fc11ae.jpg" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 1</h2>
                   <p>ietur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -131,7 +131,7 @@ const Home = () => {
             <div className="box">
               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn4OV2HGXnUgokhujx8qz2_W_kG-gW3pfPkg&usqp=CAU" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 2</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -140,7 +140,7 @@ const Home = () => {
             <div className="box">
               <img src="https://www.ulampinoy.com/static/images/chopsuey-cooking-wok.jpg?nf_resize=fit&w=960" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 3</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -150,7 +150,7 @@ const Home = () => {
             <div className="box">
               <img src="https://pbs.twimg.com/media/C3UqzZ4WMAAe1DG.jpg" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 4</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -159,7 +159,7 @@ const Home = () => {
             <div className="box">
               <img src="https://i0.wp.com/hicaps.com.ph/wp-content/uploads/2022/07/sinigang-e1658714142155.jpg?resize=350%2C200&ssl=1" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 5</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -168,7 +168,7 @@ const Home = () => {
             <div className="box">
               <img src="https://www.knorr.com/content/dam/unilever/knorr_world/philippines/photography_and_pictures/pinoy-ulam-list-adobo-85776428-jpg.jpg" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 6</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -178,7 +178,7 @@ const Home = () => {
             <div className="box">
               <img src="https://pbs.twimg.com/media/C3UqzZ4WMAAe1DG.jpg" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 4</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -187,7 +187,7 @@ const Home = () => {
             <div className="box">
               <img src="https://i0.wp.com/hicaps.com.ph/wp-content/uploads/2022/07/sinigang-e1658714142155.jpg?resize=350%2C200&ssl=1" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 5</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
@@ -196,7 +196,7 @@ const Home = () => {
             <div className="box">
               <img src="https://www.knorr.com/content/dam/unilever/knorr_world/philippines/photography_and_pictures/pinoy-ulam-list-adobo-85776428-jpg.jpg" alt="" />
               <div className="img-text">
-                <div className="content">
+                <div className="content1">
                   <h2>Food 6</h2>
                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam assumenda nulla aspernatur enim ut, minima.</p>
                 </div>
